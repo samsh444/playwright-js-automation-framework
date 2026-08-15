0f100f47da1c200b5faa3bc05bb585a6ed1e6cfb
Samuel Shaw <shawsa65@gmail.com>
2026-08-15 09:35:25 -0600

    modded tests/getByText.spec.js

---
+++ tests/getByText.spec.js
@@ -0,0 +1,11 @@
+//import test and expect from playwright/test
+import {test, expect} from '@playwright/test';
+
+//set up test for going to website
+test('Launch application', async({page}) => {
+    await page.goto('https://login.salesforce.com/');
+
+    //getRoleBy for playwright locators > Username textbox/field and click method 
+    await page.getByRole('textbox', {name: 'Username'}).click();
+
+})
